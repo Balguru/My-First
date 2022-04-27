220ac7c15b2d410a12b510bb312071744f12f936
Balguru <chbalaguru@gmail.com>
2022-04-27 18:36:24 +0530

    all data changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import routeArray from "./Components/Data";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
+import About from "./Components/About";
 
 function App() {
   return (
-    <div>
-<h1>Hello React Redux</h1>
+    <Router>
+      <div style={{backgroundColor:"orange",width:"10%",height:"100%"}}>
+       
+          {routeArray.map((route, i) => (
+            <h2 key={i} style={{backgroundColor:"blue",textDecoration:"none"}}>
+              <Link key={i} to={route.path}>
+                {route.Title}
+              </Link>
+            </h2>
+          ))}
+     
+        <hr />
 
-    </div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          {routeArray.map((route, i) => (
+            <Route key={i} path={route.path} element={route.component} />
+          ))}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Data.js
@@ -0,0 +1,30 @@
+const routeArray = [
+  {
+    path: "/",
+    component: "Home",
+    Title: "Home",
+  },
+  {
+    path: "/about",
+    component: "About",
+    Title: "About",
+  },
+
+  {
+    path: "/contact",
+    component: "Contact",
+    Title: "Contact",
+  },
+  {
+    path: "/service",
+    component: "Service",
+    Title: "Service",
+  },
+  {
+    path: "/profiles",
+    component: "Profiles",
+    Title: "Profiles",
+  },
+];
+
+export default routeArray;
